fe2771227215822ff807952f22098b505ef32bbe
Andrew Brown <andrew.brown@miriamtech.com>
2024-12-30 19:07:17 -0800

    remove accidental tests

--- tests/integration/test_graph_integration.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from asyncua.sync import Server
-
-
-def test_restart_timer(client):
-    client.ui.spinBoxNumberOfPoints.setValue(90)
-    client.ui.buttonApply.click()
-    assert client.graph_ui.N == 90
